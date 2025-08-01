bc35913c928d9a718e4341a450b1a67b8d582fce
Owlface Games <108976740+OwlfaceGames@users.noreply.github.com>
2025-08-02 00:45:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	bool creating_files = false;
 
-	char *first_arg = argv[1];
+	if (argc > 2) {
+		creating_files = true;
+	} else {
+		printf("Usage: btouch <.extension> <file1> <file2> ...\n");
+		printf("Note: ommit extension by leaving a space there instead.\n");
+		return 1;
+	}
 
-	if (argc > 1) {
-		if (!strcmp(&first_arg[0], ".")) {
-			creating_files = true;
+	char user_input[256];
+
+	if (creating_files) {
+		for (int i = 2; i < argc; i++) {
+			snprintf(user_input, sizeof(user_input), "touch %s%s", argv[i], argv[1]);
+			system(user_input);
 		}
 	}
 
-	if (creating_files) {
-		printf("Creating files...\n");
-	}
+	system("sudo mv btouch /usr/local/bin");
+	system("sudo chmod +x /usr/local/bin/btouch");
 
 	return 0;
 }
